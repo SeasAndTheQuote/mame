147058d6e355d9a8920cff52b0dd3c9b191bf747
KubaMilion <jakub.grzegorczyk@gmail.com>
2019-09-08 21:49:47 +0200

    app init

---
+++ src/com/bruno/Kid.java
@@ -0,0 +1,13 @@
+package com.bruno;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Kid {
+
+	public Kid() {}
+
+	List<Integer> availability = new ArrayList<>();
+
+
+}
--- src/com/bruno/Main.java
+++ src/com/bruno/Main.java
@@ -1,8 +1,14 @@
 package com.bruno;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
-    }
+	public static void main(String[] args) {
+
+		Map<Integer, String> freeHours = new HashMap<Integer, String>();
+
+
+	}
 }
